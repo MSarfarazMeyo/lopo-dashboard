1875a1cdad42d2de6bf45aac671f1f75d35b0626
MSarfarazMeyo <msarfaraz.meyo@gmail.com>
2024-11-20 20:25:19 +0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route, useNavigate } from "react-router-dom";
 import './App.css';
 import GoogleLoginButton from './components/auth/google-login-button';
+import Login from './pages/Login';
 
 function App() {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+
+    navigate("/login"); // Redirect to login if not authenticated
+
+  }, [navigate]);
+
   return (
 
     <div className="App font-opensans">
 
 
       <Routes>
-        <Route index path="/" element={<GoogleLoginButton />} />
+        <Route index path="/login" element={<Login />} />
+
+        {/* <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<Admin />} />
+          <Route path="comments" element={<Comments />} />
+          <Route path="posts/manage" element={<ManagePosts />} />
+          <Route path="posts/manage/edit/:slug" element={<EditPost />} />
+          <Route path="/admin/chat" element={<ChatScreen />} />
+
+          <Route path="/admin/doctors/manage" element={<ManageDoctors />} />
+          <Route path="/admin/doctor/profile" element={<EditDoctor />} />
+
+          <Route path="/admin/manager/manage" element={<ManageManagers />} />
+          <Route path="/admin/manager/profile" element={<EditManager />} />
+
+          <Route path="categories/manage" element={<Categories />} />
+          <Route
+            path="categories/manage/edit/:slug"
+            element={<EditCategories />}
+          />
+          <Route path="users/manage" element={<Users />} />
+        </Route> */}
       </Routes>
+
     </div>
 
   );
---
+++ src/pages/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import GoogleLoginButton from '../components/auth/google-login-button'
+
+export const Login = () => {
+    return (
+        <div>
+            <GoogleLoginButton />
+
+        </div>
+    )
+}
+
+export default Login
